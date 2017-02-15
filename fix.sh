be6af4a8544691d6cbfde55a2edfc283d3523877
Shakib-Bin Hamid <sh3g12@soton.ac.uk>
2017-02-15 20:33:21 +0000

    fixing the dept names

--- fix.sh
+++ fix.sh
@@ -3,3 +3,5 @@ grep -v "Total,,,,,," Projects_CW1_MessedUp.csv > removing_total.csv
 grep "^,,,,," removing_total.csv | wc -l
 grep -v "^,,,,,,," removing_total.csv > removing_empty.csv
 sort removing_empty.csv | sort | uniq -d
+
+if(value.split(",").length() > 1, value.split(",")[0] + value.split(",")[1], value.split(",")[0])

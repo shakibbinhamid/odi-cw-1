6ba12479a09f46cfe9f5a596cc6cbf91eec6fe71
Shakib-Bin Hamid <sh3g12@soton.ac.uk>
2017-02-22 16:13:17 +0000

    reduced height

--- www/js/vis-1.js
+++ www/js/vis-1.js
@@ -16,7 +16,7 @@ var defaults = {
     format: ".2f",
     title: "",
     width: 1000,
-    height: 800
+    height: 650
 };
 
 function main(o, data) {
@@ -99,23 +99,12 @@ function main(o, data) {
         root.depth = 0;
     }
 
-    // Aggregate the values for internal nodes. This is normally done by the
-    // treemap layout, but not here because of our custom implementation.
-    // We also take a snapshot of the original children (_children) to avoid
-    // the children being overwritten when when layout is computed.
     function accumulate(d) {
         return (d._children = d.values)
             ? d.projected_cost = d.values.reduce(function(p, v) { return p + accumulate(v); }, 0)
             : d.projected_cost;
     }
 
-    // Compute the treemap layout recursively such that each group of siblings
-    // uses the same size (1×1) rather than the dimensions of the parent cell.
-    // This optimizes the layout for the current zoom state. Note that a wrapper
-    // object is created for the parent node for each group of siblings so that
-    // the parent’s dimensions are not discarded as we recurse. Since each group
-    // of sibling was laid out in 1×1, we must rescale to fit using absolute
-    // coordinates. This lets us use a viewport to zoom.
     function layout(d) {
         if (d._children) {
             treemap.nodes({_children: d._children});

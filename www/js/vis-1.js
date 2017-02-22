64779dea22fe5eb21ce1f4ff830fa5e93a5cdc3a
Shakib-Bin Hamid <sh3g12@soton.ac.uk>
2017-02-22 13:27:02 +0000

    the treemap works

--- www/js/vis-1.js
+++ www/js/vis-1.js
@@ -29,7 +29,18 @@ function main(o, data) {
 
     $('#chart').width(opts.width).height(opts.height);
     var width = opts.width - margin.left - margin.right,
-        height = opts.height - margin.top - margin.bottom - theight;
+        height = opts.height - margin.top - margin.bottom - theight,
+        transitioning;
+
+    var color = d3.scale.category20c();
+
+    var x = d3.scale.linear()
+        .domain([0, width])
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .domain([0, height])
+        .range([0, height]);
 
     var treemap = d3.layout.treemap()
         .children(function(d, depth) { return depth ? null : d._children; })
@@ -37,6 +48,32 @@ function main(o, data) {
         .sort(function(a, b) { return a.projected_cost - b.projected_cost; })
         .ratio(height / width * 0.5 * (1 + Math.sqrt(5)))
         .round(false);
+
+    var svg = d3.select("#chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.bottom + margin.top)
+        .style("margin-left", -margin.left + "px")
+        .style("margin.right", -margin.right + "px")
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .style("shape-rendering", "crispEdges");
+
+    var grandparent = svg.append("g")
+        .attr("class", "grandparent");
+
+    grandparent.append("rect")
+        .attr("y", -margin.top)
+        .attr("width", width)
+        .attr("height", margin.top);
+
+    grandparent.append("text")
+        .attr("x", 6)
+        .attr("y", 6 - margin.top)
+        .attr("dy", ".75em");
+
+    if (opts.title) {
+        $("#chart").prepend("<p class='title'>" + opts.title + "</p>");
+    }
 
     if (data instanceof Array) {
         root = { key: rname, values: data };
@@ -47,9 +84,13 @@ function main(o, data) {
     initialize(root);
     accumulate(root);
     layout(root);
-
     console.log(root);
-    console.log(opts);
+    display(root);
+
+    if (window.parent !== window) {
+        var myheight = document.documentElement.scrollHeight || document.body.scrollHeight;
+        window.parent.postMessage({height: myheight}, '*');
+    }
 
     function initialize(root) {
         root.x = root.y = 0;
@@ -87,6 +128,123 @@ function main(o, data) {
                 layout(c);
             });
         }
+    }
+
+    function display(d) {
+        grandparent
+            .datum(d.parent)
+            .on("click", transition)
+            .select("text")
+            .text(name(d));
+
+        var g1 = svg.insert("g", ".grandparent")
+            .datum(d)
+            .attr("class", "depth");
+
+        var g = g1.selectAll("g")
+            .data(d._children)
+            .enter().append("g");
+
+        g.filter(function(d) { return d._children; })
+            .classed("children", true)
+            .on("click", transition);
+
+        var children = g.selectAll(".child")
+            .data(function(d) { return d._children || [d]; })
+            .enter().append("g");
+
+        children.append("rect")
+            .attr("class", "child")
+            .call(rect)
+            .append("title")
+            .text(function(d) { return d.key + " (" + formatNumber(d.projected_cost) + ")"; });
+        children.append("text")
+            .attr("class", "ctext")
+            .text(function(d) { return d.key; })
+            .call(text2);
+
+        g.append("rect")
+            .attr("class", "parent")
+            .call(rect);
+
+        var t = g.append("text")
+            .attr("class", "ptext")
+            .attr("dy", ".75em")
+
+        t.append("tspan")
+            .text(function(d) { return d.key; });
+        t.append("tspan")
+            .attr("dy", "1.0em")
+            .text(function(d) { return formatNumber(d.projected_cost); });
+        t.call(text);
+
+        g.selectAll("rect")
+            .style("fill", function(d) { return color(d.key); });
+
+        function transition(d) {
+            if (transitioning || !d) return;
+            transitioning = true;
+
+            var g2 = display(d),
+                t1 = g1.transition().duration(750),
+                t2 = g2.transition().duration(750);
+
+            // Update the domain only after entering new elements.
+            x.domain([d.x, d.x + d.dx]);
+            y.domain([d.y, d.y + d.dy]);
+
+            // Enable anti-aliasing during the transition.
+            svg.style("shape-rendering", null);
+
+            // Draw child nodes on top of parent nodes.
+            svg.selectAll(".depth").sort(function(a, b) { return a.depth - b.depth; });
+
+            // Fade-in entering text.
+            g2.selectAll("text").style("fill-opacity", 0);
+
+            // Transition to the new view.
+            t1.selectAll(".ptext").call(text).style("fill-opacity", 0);
+            t1.selectAll(".ctext").call(text2).style("fill-opacity", 0);
+            t2.selectAll(".ptext").call(text).style("fill-opacity", 1);
+            t2.selectAll(".ctext").call(text2).style("fill-opacity", 1);
+            t1.selectAll("rect").call(rect);
+            t2.selectAll("rect").call(rect);
+
+            // Remove the old node when the transition is finished.
+            t1.remove().each("end", function() {
+                svg.style("shape-rendering", "crispEdges");
+                transitioning = false;
+            });
+        }
+
+        return g;
+    }
+
+    function text(text) {
+        text.selectAll("tspan")
+            .attr("x", function(d) { return x(d.x) + 6; })
+        text.attr("x", function(d) { return x(d.x) + 6; })
+            .attr("y", function(d) { return y(d.y) + 6; })
+            .style("opacity", function(d) { return this.getComputedTextLength() < x(d.x + d.dx) - x(d.x) ? 1 : 0; });
+    }
+
+    function text2(text) {
+        text.attr("x", function(d) { return x(d.x + d.dx) - this.getComputedTextLength() - 6; })
+            .attr("y", function(d) { return y(d.y + d.dy) - 6; })
+            .style("opacity", function(d) { return this.getComputedTextLength() < x(d.x + d.dx) - x(d.x) ? 1 : 0; });
+    }
+
+    function rect(rect) {
+        rect.attr("x", function(d) { return x(d.x); })
+            .attr("y", function(d) { return y(d.y); })
+            .attr("width", function(d) { return x(d.x + d.dx) - x(d.x); })
+            .attr("height", function(d) { return y(d.y + d.dy) - y(d.y); });
+    }
+
+    function name(d) {
+        return d.parent
+            ? name(d.parent) + " / " + d.key + " (" + formatNumber(d.projected_cost) + ")"
+            : d.key + " (" + formatNumber(d.projected_cost) + ")";
     }
 }
 

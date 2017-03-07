c5b94f96372b17ed729d4624771f7f93df482176
shakib <sh3g12@soton.ac.uk>
2017-03-07 15:45:20 +0000

    changed the routes so that they use pretty url.

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ app.use(express.static(__dirname + '/www'));
 
 app.set('port', (process.env.PORT || 5000));
 
+app.get('/', function(req, res) {
+    res.redirect('/parcoords')
+});
+
+app.get('/parcoords', function(req, res) {
+    res.sendFile(__dirname + '/www/parcoords.html');
+});
+
+app.get('/treemap', function(req, res) {
+    res.sendFile(__dirname + '/www/treemap.html');
+});
+
+app.get('/cleaning', function(req, res) {
+    res.sendFile(__dirname + '/www/cleaning.html');
+});
+
 app.listen(app.get('port'), function () {
     console.log('Example app listening on port ' + (process.env.PORT || 5000))
 });
